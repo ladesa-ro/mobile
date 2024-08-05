b471d90fe51b97f23a262acb2e0218745123c682
Hugo Santos <139011452+hugh514@users.noreply.github.com>
2024-08-05 19:04:38 -0400

    bugs: Corrigindo a requisição para o parâmetro correto

--- sisgha/lib/app/widgets/builder_perfil.dart
+++ sisgha/lib/app/widgets/builder_perfil.dart
@@ -3,6 +3,7 @@ import 'package:sisgha/app/api/repository.dart';
 import 'package:sisgha/app/constants/colors.dart';
 import 'package:sisgha/app/constants/tamanhoTela.dart';
 import 'package:sisgha/app/model/userModel.dart';
+
 import 'package:sisgha/app/widgets/progressIndicator.dart';
 
 class FutureBuilderPerfil extends StatefulWidget {
@@ -16,7 +17,7 @@ class _FutureBuilderPerfilState extends State<FutureBuilderPerfil> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<UserModel>(
-      future: buscarUser(context), // Aqui pode estar o problema
+      future: buscarUser(context),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return Center(
@@ -24,46 +25,55 @@ class _FutureBuilderPerfilState extends State<FutureBuilderPerfil> {
           );
         }
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
+          return Center(
+            child: const CircularProgressIndicator(),
           );
         }
         if (snapshot.hasData) {
-          UserModel user = snapshot.data!;
-          return Container(
-            height: 200,
-            width: TamanhoTela.horizontal(context),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Stack(
-                  children: [
-                    Positioned(
-                      child: Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: ColorApp.Branco,
-                        ),
-                        height: 105,
-                        width: 105,
-                        child: const CircleAvatar(
-                          backgroundColor: Colors.transparent,
-                          backgroundImage: NetworkImage(
-                            "https://luna.sisgha.com/api/usuarios/imagem/perfil",
+          UserModel user = snapshot.data!; 
+              //se funcionar, mostra isso:
+              // ignore: sized_box_for_whitespace
+              return Container(
+                  height: 200,
+                  width: TamanhoTela.horizontal(context),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Stack(
+                        children: [
+                          Positioned(
+                            child: Container(
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: ColorApp.Branco),
+                              height: 105,
+                              width: 105,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: NetworkImage(
+                                  "https://luna.sisgha.com/api/usuarios/$id/imagem/perfil",
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                        ],
+                      )
+                    ],
+                  )
+
+                  //   title: Text(snapshot.data!["nome"]),
+                  //   subtitle: Text(snapshot.data!["email"]),
+                  //   trailing:
+                  //       Text("Matricula: \n${snapshot.data!["matriculaSiape"]}"),
+
+                  );
+            },
           );
         }
         return const Center(
-          child: ProgressIndicator(
+          child: Progressindicator(
             tamanho: 100,
           ),
         );
