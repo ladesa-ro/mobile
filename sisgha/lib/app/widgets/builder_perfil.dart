da649f5dd0bf60e3cf83c220c2b0d9abbd2d19ce
Hugo Santos <139011452+hugh514@users.noreply.github.com>
2024-08-05 19:03:07 -0400

    feat: Mudando os parâmetros da Api

--- sisgha/lib/app/widgets/builder_perfil.dart
+++ sisgha/lib/app/widgets/builder_perfil.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sisgha/app/api/repository.dart';
 import 'package:sisgha/app/constants/colors.dart';
 import 'package:sisgha/app/constants/tamanhoTela.dart';
-
+import 'package:sisgha/app/model/userModel.dart';
 import 'package:sisgha/app/widgets/progressIndicator.dart';
 
 class FutureBuilderPerfil extends StatefulWidget {
@@ -15,74 +15,55 @@ class FutureBuilderPerfil extends StatefulWidget {
 class _FutureBuilderPerfilState extends State<FutureBuilderPerfil> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map<String, dynamic>>(
-      future: buscarUser(context),
+    return FutureBuilder<UserModel>(
+      future: buscarUser(context), // Aqui pode estar o problema
       builder: (context, snapshot) {
         if (snapshot.hasError) {
+          return Center(
+            child: const Text("Erro ao carregar Usuário"),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(
-            child: Text("Erro ao carregar Usuário"),
+            child: CircularProgressIndicator(),
           );
         }
         if (snapshot.hasData) {
-          return FutureBuilder<String?>(
-            future: pegarId(),
-            builder: (context, idSnapshot) {
-              if (idSnapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: Progressindicator(
-                    tamanho: 100,
-                  ),
-                );
-              }
-              if (idSnapshot.hasError || idSnapshot.data == null) {
-                return const Center(
-                  child: Text("Erro ao obter ID do usuário"),
-                );
-              }
-              var id = idSnapshot.data!;
-              //se funcionar, mostra isso:
-              // ignore: sized_box_for_whitespace
-              return Container(
-                  height: 200,
-                  width: TamanhoTela.horizontal(context),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Stack(
-                        children: [
-                          Positioned(
-                            child: Container(
-                              padding: const EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(100),
-                                  color: ColorApp.Branco),
-                              height: 105,
-                              width: 105,
-                              child: CircleAvatar(
-                                backgroundColor: Colors.transparent,
-                                backgroundImage: NetworkImage(
-                                  "https://luna.sisgha.com/api/usuarios/$id/imagem/perfil",
-                                ),
-                              ),
-                            ),
+          UserModel user = snapshot.data!;
+          return Container(
+            height: 200,
+            width: TamanhoTela.horizontal(context),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Positioned(
+                      child: Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: ColorApp.Branco,
+                        ),
+                        height: 105,
+                        width: 105,
+                        child: const CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: NetworkImage(
+                            "https://luna.sisgha.com/api/usuarios/imagem/perfil",
                           ),
-                        ],
-                      )
-                    ],
-                  )
-
-                  //   title: Text(snapshot.data!["nome"]),
-                  //   subtitle: Text(snapshot.data!["email"]),
-                  //   trailing:
-                  //       Text("Matricula: \n${snapshot.data!["matriculaSiape"]}"),
-
-                  );
-            },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           );
         }
         return const Center(
-          child: Progressindicator(
+          child: ProgressIndicator(
             tamanho: 100,
           ),
         );
