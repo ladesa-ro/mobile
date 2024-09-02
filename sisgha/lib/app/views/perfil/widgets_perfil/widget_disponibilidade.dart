f3d279704a32a98c4a8229693e60ae087ed918d0
Yuri Gabriel <yurigabrielsouza10@gmail.com>
2024-09-01 21:46:04 -0400

    feat: adicionando o componete de trocar de dia no campo disponibilidae :sparkles:

--- sisgha/lib/app/views/perfil/widgets_perfil/widget_disponibilidade.dart
+++ sisgha/lib/app/views/perfil/widgets_perfil/widget_disponibilidade.dart
@@ -1,144 +1,160 @@
-import "package:flutter/material.dart";
-import "package:sisgha/app/constants/colors.dart";
-import "package:sisgha/app/constants/estilos.dart";
-import "package:sisgha/app/views/perfil/widgets_perfil/widget_sair.dart";
+import 'package:flutter/material.dart';
+import 'package:sisgha/app/constants/colors.dart';
+import 'package:sisgha/app/constants/estilos.dart';
+import 'package:sisgha/app/views/perfil/widgets_perfil/widget_sair.dart';
 
-Widget disponibilidade() {
-  return LayoutBuilder(
-    builder: (context, constraints) => Padding(
-      padding: const EdgeInsets.only(top: 15),
-      child: ListView(
+class WidgetDisponibilidade extends StatefulWidget {
+  const WidgetDisponibilidade({super.key});
+
+  @override
+  State<WidgetDisponibilidade> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<WidgetDisponibilidade> {
+  List<String> dias = [
+    'Segunda-Feira',
+    'Terça-Feira',
+    'Quarta-Feira',
+    'Quinta-Feira',
+    'Sexta-Feira'
+  ];
+
+  int diaIndex = 0;
+
+  void _proximoDia() {
+    setState(() {
+      if (diaIndex < dias.length - 1) {
+        diaIndex++;
+      }
+    });
+  }
+
+  void _diaAnterior() {
+    setState(() {
+      if (diaIndex > 0) {
+        diaIndex--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
         physics: const NeverScrollableScrollPhysics(),
         children: [
+          const SizedBox(height: 15),
           Container(
-            margin: tamanho(constraints.maxWidth),
-            height: 200,
+            decoration: BoxDecoration(
+              color: ColorApp.VerdePrincipal,
+              borderRadius: BorderRadius.circular(10),
+            ),
             width: constraints.maxWidth,
-            decoration: decoration(),
+            height: 55,
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('07:30 - 08:20', style: estilo()),
-                    Text('08:20 - 09:10', style: estilo()),
-                    Text('09:10 - 10:00', style: estilo()),
-                    Text('10:20 - 11:10', style: estilo()),
-                    Text('11:10 - 12:00', style: estilo()),
-                  ],
+                IconButton(
+                  onPressed: _diaAnterior,
+                  icon: const Icon(
+                    Icons.arrow_left,
+                    size: 40,
+                    color: ColorApp.Branco,
+                  ),
                 ),
                 const Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Matutino',
-                      style: estiloTexto(18,
-                          cor: ColorApp.VerdePrincipal, peso: FontWeight.bold),
-                    )
-                  ],
+                Text(
+                  dias[diaIndex],
+                  style: estiloTexto(15,
+                      cor: ColorApp.Branco, peso: FontWeight.bold),
                 ),
                 const Spacer(),
+                IconButton(
+                  onPressed: _proximoDia,
+                  icon: const Icon(
+                    Icons.arrow_right,
+                    size: 40,
+                    color: ColorApp.Branco,
+                  ),
+                ),
               ],
             ),
           ),
-          Container(
-            margin: tamanho(constraints.maxWidth),
-            height: 200,
-            width: constraints.maxWidth,
-            decoration: decoration(),
-            child: Row(
-              children: [
-                const Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('13:00 - 13:50', style: estilo()),
-                    Text('13:50 - 14:40', style: estilo()),
-                    Text('14:40 - 15:30', style: estilo()),
-                    Text('15:50 - 16:40', style: estilo()),
-                    Text('16:40 - 17:30', style: estilo()),
-                  ],
-                ),
-                const Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Vespertino',
-                      style: estiloTexto(18,
-                          cor: ColorApp.VerdePrincipal, peso: FontWeight.bold),
-                    )
-                  ],
-                ),
-                const Spacer(),
-              ],
-            ),
-          ),
-          Container(
-            margin: tamanho(constraints.maxWidth),
-            height: 200,
-            width: constraints.maxWidth,
-            decoration: decoration(),
-            child: Row(
-              children: [
-                const Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('19:00 - 19:50', style: estilo()),
-                    Text('19:50 - 20:40', style: estilo()),
-                    Text('20:40 - 21:30', style: estilo()),
-                    Text('21:50 - 22:40', style: estilo()),
-                    Text('22:40 - 23:30', style: estilo()),
-                  ],
-                ),
-                const Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Noturno',
-                      style: estiloTexto(18,
-                          cor: ColorApp.VerdePrincipal, peso: FontWeight.bold),
-                    )
-                  ],
-                ),
-                const Spacer(),
-              ],
-            ),
-          ),
+          const SizedBox(height: 15),
+          _buildHorario(constraints.maxWidth, '$diaIndex', [
+            '07:30 - 08:20',
+            '08:20 - 09:10',
+            '09:10 - 10:00',
+            '10:20 - 11:10',
+            '11:10 - 12:00',
+          ]),
+          _buildHorario(constraints.maxWidth, 'Vespertino', [
+            '13:00 - 13:50',
+            '13:50 - 14:40',
+            '14:40 - 15:30',
+            '15:50 - 16:40',
+            '16:40 - 17:30',
+          ]),
+          _buildHorario(constraints.maxWidth, 'Noturno', [
+            '19:00 - 19:50',
+            '19:50 - 20:40',
+            '20:40 - 21:30',
+            '21:50 - 22:40',
+            '22:40 - 23:30',
+          ]),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               widgetQuit(context),
             ],
           ),
-          const SizedBox(
-            height: 15,
-          )
+          const SizedBox(height: 15),
         ],
       ),
-    ),
-  );
-}
+    );
+  }
 
-EdgeInsets tamanho(double largura) {
-  return EdgeInsets.only(
-    bottom: 25,
-    left: largura * 0.08,
-    right: largura * 0.08,
-  );
-}
+  Widget _buildHorario(double width, String periodo, List<String> horarios) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 25),
+      height: 200,
+      width: width,
+      decoration: decoration(),
+      child: Row(
+        children: [
+          const Spacer(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: horarios
+                .map((horario) => Text(horario, style: estilo()))
+                .toList(),
+          ),
+          const Spacer(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                periodo,
+                style: estiloTexto(18,
+                    cor: ColorApp.VerdePrincipal, peso: FontWeight.bold),
+              )
+            ],
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
 
-BoxDecoration decoration() {
-  return BoxDecoration(
-    borderRadius: BorderRadius.circular(20),
-    color: ColorApp.CorFundoDisponibilidade,
-  );
-}
+  BoxDecoration decoration() {
+    return BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      color: ColorApp.CorFundoDisponibilidade,
+    );
+  }
 
-TextStyle estilo() {
-  return estiloTexto(17, cor: ColorApp.VerdePrincipal, peso: FontWeight.bold);
+  TextStyle estilo() {
+    return estiloTexto(17, cor: ColorApp.VerdePrincipal, peso: FontWeight.bold);
+  }
 }
