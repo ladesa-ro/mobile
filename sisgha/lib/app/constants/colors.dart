fee987dac5058f2104e2bcb223bfb60892e9ecf7
Yuri Gabriel <yurigabrielsouza10@gmail.com>
2024-09-14 17:52:27 -0400

    style: finalizado a estilização do mini calendario :sparkles:

--- sisgha/lib/app/constants/colors.dart
+++ sisgha/lib/app/constants/colors.dart
@@ -6,7 +6,7 @@ class ColorApp {
   static const Color VerdePrincipal = Color.fromRGBO(57, 160, 72, 1);
   static const Color VerdeClaro = Color.fromRGBO(27, 186, 71, 1);
   static const Color VerdeEscuro = Color.fromRGBO(17, 141, 59, 1);
-  static const Color VerdeCinza = Color.fromRGBO(154, 182, 158, 1);
+  static const Color VerdeCinza = Color.fromARGB(186, 154, 182, 158);
   static const Color Branco = Color.fromRGBO(255, 255, 255, 1);
   static const Color Preto = Color(0xFF000000);
   static const Color RoxoEscuro = Color.fromRGBO(83, 0, 166, 1);
--- sisgha/lib/app/widgets/mini_calend.dart
+++ sisgha/lib/app/widgets/mini_calend.dart
@@ -23,7 +23,7 @@ class _MiniCalendarioState extends State<MiniCalendario> {
       insetPadding: EdgeInsets.zero,
       child: SizedBox(
         width: TamanhoTela.horizontal(context) - 50,
-        height: TamanhoTela.vertical(context) * 0.40,
+        height: 350,
         child: TableCalendar(
           firstDay: DatasFormatadas.primeiroDiaDoAno,
           lastDay: DatasFormatadas.ultimoDiaDoAno,
@@ -31,140 +31,38 @@ class _MiniCalendarioState extends State<MiniCalendario> {
           calendarFormat: CalendarFormat.month,
           locale: 'pt-BR',
           shouldFillViewport: true,
-          daysOfWeekHeight: 25,
-          daysOfWeekStyle: estiloParteSuperior(),
-          headerStyle: estiloCabessario(),
+          daysOfWeekHeight: 23,
+          daysOfWeekStyle: _estiloParteSuperior(),
+          headerStyle: _estiloCabessario(),
           calendarBuilders: _calendarBuilder(),
-        ),
-      ),
-    );
-  }
-
-  HeaderStyle estiloCabessario() {
-    return HeaderStyle(
-      headerMargin: const EdgeInsets.only(bottom: 15),
-      headerPadding: const EdgeInsets.all(5),
-      decoration: const BoxDecoration(
-        color: ColorApp.RoxoEscuro,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(15),
-          topRight: Radius.circular(15),
-        ),
-      ),
-      titleCentered: true,
-      formatButtonVisible: false,
-      titleTextStyle:
-          estiloTexto(20, cor: ColorApp.Branco, peso: FontWeight.bold),
-      leftChevronIcon: const Iconify(
-        Icones.setaEsquerda,
-        color: ColorApp.Branco,
-        size: 30,
-      ),
-      rightChevronIcon: const Iconify(
-        Icones.setaDireita,
-        color: ColorApp.Branco,
-        size: 30,
-      ),
-      titleTextFormatter: (date, locale) =>
-          DateFormat.MMMM(locale).format(date).toUpperCase(),
-    );
-  }
-
-  DaysOfWeekStyle estiloParteSuperior() {
-    return DaysOfWeekStyle(
-      weekdayStyle: estiloTexto(15, peso: FontWeight.bold, cor: ColorApp.Preto),
-      weekendStyle: estiloTexto(15, peso: FontWeight.bold, cor: ColorApp.Preto),
-      dowTextFormatter: (date, locale) {
-        switch (date.weekday) {
-          case DateTime.monday:
-            return 'Seg';
-          case DateTime.tuesday:
-            return 'Ter';
-          case DateTime.wednesday:
-            return 'Qua';
-          case DateTime.thursday:
-            return 'Qui';
-          case DateTime.friday:
-            return 'Sex';
-          case DateTime.saturday:
-            return 'Sab';
-          case DateTime.sunday:
-            return 'Dom';
-          default:
-            return '';
-        }
-      },
-    );
-  }
-
-  CalendarBuilders _calendarBuilder() {
-    Color color = Colors.black;
-    return CalendarBuilders(
-        defaultBuilder: (context, date, events) {
-          final now = DateTime.now();
-
-          if (date.isAfter(DateTime(now.year, 2, 7)) &&
-              date.isBefore(DateTime(now.year, 4, 1))) {
-            color = const Color(0xFF08C17F);
-          } else if (date.isAfter(DateTime(now.year, 7, 22)) &&
-              date.isBefore(DateTime(now.year, 9, 25))) {
-            color = const Color(0xFFB20A33);
-          } else if (date.isAfter(DateTime(now.year, 9, 24)) &&
-              date.isBefore(DateTime(now.year, 11, 29))) {
-            color = Colors.amber;
-          } else if (date.isAfter(DateTime(now.year, 4, 17)) &&
-              date.isBefore(DateTime(now.year, 6, 20))) {
-            color = const Color.fromARGB(255, 69, 0, 158);
-          } else if (date.isAfter(DateTime(now.year, 6, 19)) &&
-              date.isBefore(DateTime(now.year, 7, 23))) {
-            color = Colors.redAccent;
-          } else {
-            const TextStyle(color: Colors.transparent);
-            color = const Color.fromRGBO(217, 217, 217, 100);
-          }
-          return _estiloDosBlocosDosDiasDoMes(color, date);
-        },
-        todayBuilder: (context, day, focusedDay) =>
-            estiloDoBlocoDoDiaDeHoje(context, day, focusedDay));
-  }
-
-  Widget _estiloDosBlocosDosDiasDoMes(Color color, DateTime day) {
-    return Container(
-      width: 40,
-      height: 40,
-      margin: const EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Center(
-        child: Text(
-          '${day.day}',
-          style: _estiloTextoNumeros(),
+          pageAnimationCurve: Curves.linear,
+          pageAnimationDuration: const Duration(milliseconds: 500),
         ),
       ),
     );
   }
 }
 
-Widget estiloDoBlocoDoDiaDeHoje(
-    BuildContext context, DateTime day, DateTime focusedDay) {
+Widget _estiloDoBlocoDoDiaDeHoje(
+    BuildContext context, DateTime day, Color cor) {
   return Container(
     width: double.maxFinite,
     height: double.maxFinite,
-    margin: const EdgeInsets.all(5),
-    decoration: const BoxDecoration(
-      color: Color.fromARGB(255, 69, 0, 158),
-      borderRadius: BorderRadius.all(Radius.circular(10)),
+    margin: const EdgeInsets.all(7),
+    decoration: BoxDecoration(
+      color: cor,
+      borderRadius: const BorderRadius.all(
+        Radius.circular(10),
+      ),
     ),
     alignment: Alignment.center,
     child: Padding(
       padding: const EdgeInsets.all(2.0),
       child: Container(
         decoration: BoxDecoration(
-          border: Border.all(color: ColorApp.Branco, width: 1),
+          border: Border.all(color: ColorApp.Branco, width: 1.5),
           borderRadius: const BorderRadius.all(
-            Radius.circular(8),
+            Radius.circular(10),
           ),
         ),
         child: Center(
@@ -178,6 +76,127 @@ Widget estiloDoBlocoDoDiaDeHoje(
   );
 }
 
+Widget _estiloDosBlocosDosDiasDoMes(Color color, DateTime day) {
+  return Container(
+    width: double.maxFinite,
+    height: double.maxFinite,
+    margin: const EdgeInsets.all(7),
+    decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Center(
+      child: Text(
+        '${day.day}',
+        style: _estiloTextoNumeros(),
+      ),
+    ),
+  );
+}
+
+Widget _estiloDosBlocosDosDiasDoMesDesabilitados() {
+  return Container(
+    width: double.maxFinite,
+    height: double.maxFinite,
+    margin: const EdgeInsets.all(7),
+    decoration: BoxDecoration(
+      color: ColorApp.VerdeCinza,
+      borderRadius: BorderRadius.circular(10),
+    ),
+  );
+}
+
 TextStyle _estiloTextoNumeros() {
   return estiloTexto(15, cor: ColorApp.Branco, peso: FontWeight.bold);
+}
+
+HeaderStyle _estiloCabessario() {
+  return HeaderStyle(
+    headerMargin: const EdgeInsets.only(bottom: 7),
+    headerPadding: const EdgeInsets.all(5),
+    decoration: const BoxDecoration(
+      color: ColorApp.RoxoEscuro,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(15),
+        topRight: Radius.circular(15),
+      ),
+    ),
+    titleCentered: true,
+    formatButtonVisible: false,
+    titleTextStyle:
+        estiloTexto(20, cor: ColorApp.Branco, peso: FontWeight.bold),
+    leftChevronIcon: const Iconify(
+      Icones.setaEsquerda,
+      color: ColorApp.Branco,
+      size: 30,
+    ),
+    rightChevronIcon: const Iconify(
+      Icones.setaDireita,
+      color: ColorApp.Branco,
+      size: 30,
+    ),
+    titleTextFormatter: (date, locale) =>
+        DateFormat.MMMM(locale).format(date).toUpperCase(),
+  );
+}
+
+DaysOfWeekStyle _estiloParteSuperior() {
+  return DaysOfWeekStyle(
+    weekdayStyle: estiloTexto(15, peso: FontWeight.bold, cor: ColorApp.Preto),
+    weekendStyle: estiloTexto(15, peso: FontWeight.bold, cor: ColorApp.Preto),
+    dowTextFormatter: (date, locale) {
+      switch (date.weekday) {
+        case DateTime.monday:
+          return 'Seg';
+        case DateTime.tuesday:
+          return 'Ter';
+        case DateTime.wednesday:
+          return 'Qua';
+        case DateTime.thursday:
+          return 'Qui';
+        case DateTime.friday:
+          return 'Sex';
+        case DateTime.saturday:
+          return 'Sab';
+        case DateTime.sunday:
+          return 'Dom';
+        default:
+          return '';
+      }
+    },
+  );
+}
+
+CalendarBuilders _calendarBuilder() {
+  Color color = Colors.black;
+  return CalendarBuilders(
+    defaultBuilder: (context, date, events) {
+      final now = DateTime.now();
+
+      if (date.isAfter(DateTime(now.year, 2, 7)) &&
+          date.isBefore(DateTime(now.year, 4, 18))) {
+        color = const Color(0xFF08C17F);
+      } else if (date.isAfter(DateTime(now.year, 4, 17)) &&
+          date.isBefore(DateTime(now.year, 6, 20))) {
+        color = const Color.fromARGB(255, 69, 0, 158);
+      } else if (date.isAfter(DateTime(now.year, 6, 19)) &&
+          date.isBefore(DateTime(now.year, 7, 23))) {
+        color = Colors.redAccent;
+      } else if (date.isAfter(DateTime(now.year, 7, 22)) &&
+          date.isBefore(DateTime(now.year, 9, 25))) {
+        color = const Color(0xFFB20A33);
+      } else if (date.isAfter(DateTime(now.year, 9, 24)) &&
+          date.isBefore(DateTime(now.year, 11, 29))) {
+        color = Colors.amber;
+      } else {
+        const TextStyle(color: Colors.transparent);
+        color = const Color.fromRGBO(217, 217, 217, 100);
+      }
+      return _estiloDosBlocosDosDiasDoMes(color, date);
+    },
+    todayBuilder: (context, day, focusedDay) =>
+        _estiloDoBlocoDoDiaDeHoje(context, day, color),
+    outsideBuilder: (context, day, focusedDay) =>
+        _estiloDosBlocosDosDiasDoMesDesabilitados(),
+  );
 }
