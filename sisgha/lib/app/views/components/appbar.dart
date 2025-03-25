a363e72d625d5717a00a3ced5fad0c7eba184fd7
victor henrique <victorcardosoferreirahenrique@gmail.com>
2025-03-24 20:10:30 -0400

    fix: concertei o erro de respansividade a appbar dos prof e dos alunos :bug:
    arrumei o valor do tamanho dos icon que tava calzando o erro na tela, e adicionei uma mudança quando aperta o incon pra mudar de tema o icon muda de acordo com o tem ex: se tiver no tema claro o icon fica de um sou e se tiver no tema escuro o icon é de uma lua

--- sisgha/lib/app/views/components/appbar.dart
+++ sisgha/lib/app/views/components/appbar.dart
@@ -75,12 +75,15 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
                       builder: (ctx) => mostrarDialogoDeTrocaDeTema(
                           ctx,
                           temaProvider.temaAtivo.brightness == Brightness.light
-                              ? "claro"
-                              : "escuro"));
+                              ? "escuro"
+                              : "claro"));
                 },
                 icon: Iconify(
-                  Icones.sol,
-                  size: 40,
+                  // usei a msm logica que ta ai em cima pra poder trocar o icon vi que no prototico o icon mudava de acordo com o tema ai fiz essa pequena alteração ass:victor
+                  temaProvider.temaAtivo.brightness == Brightness.light
+                      ? Icones.sol
+                      : Icones.Lapiz, // não achei o icon da lua então coloquei esse temporario :)
+                  size: 34, // mudei o tamnaho do icon pra ficar com o msm tamanho do outro ali de baixo, o tamnaho antigo tava dano um erro na tela pois olcupava muito espaço 
                   color: temaProvider.corDosIcones(),
                 )),
             if (!profOuAluno)
