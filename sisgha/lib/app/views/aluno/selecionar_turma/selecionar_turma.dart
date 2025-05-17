978990110eafe3bcbb25e5a0cc921e335dcbde68
Yuri Gabriel <yurigabrielsouza10@gmail.com>
2025-05-17 13:21:43 -0400

    test: apagado todo o codigo da tela de selecao de turmas :rotating_light:

--- sisgha/lib/app/views/aluno/selecionar_turma/selecionar_turma.dart
+++ sisgha/lib/app/views/aluno/selecionar_turma/selecionar_turma.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sisgha/app/views/aluno/selecionar_turma/widgets/app_bar_aluno.dart';
-import 'package:sisgha/app/views/aluno/selecionar_turma/widgets/body_aluno.dart';
 import 'package:sisgha/app/views/aluno/selecionar_turma/widgets/teste.dart';
 
 class PaginaAlunoSelec extends StatelessWidget {
--- sisgha/lib/app/views/aluno/selecionar_turma/widgets/teste.dart
+++ sisgha/lib/app/views/aluno/selecionar_turma/widgets/teste.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:sisgha/app/core/utils/colors.dart';
+import 'package:sisgha/app/core/utils/estilos.dart';
+import 'package:sisgha/app/core/utils/icones.dart';
+import 'package:sisgha/app/core/utils/padroes.dart';
+import 'package:sisgha/app/providers/escolha_horarios_alunos.dart';
 
 class Teste extends StatefulWidget {
   const Teste({super.key});
@@ -8,8 +15,96 @@ class Teste extends StatefulWidget {
 }
 
 class _TesteState extends State<Teste> {
+  bool formacao = true;
+  bool curso = false;
+  bool turma = false;
+  double alturaAtivado = Padroes.alturaGeral() * 0.15;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Consumer<EscolhaHorariosAlunos>(
+      builder: (context, provider, child) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ListView(
+            shrinkWrap: true,
+            physics: Padroes.efeitoDeRolagem(),
+            padding: Padroes.margem(),
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Selecionar Horario',
+                    style: estiloTexto(15, peso: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(Icones.relogio)
+                ],
+              ),
+              AnimatedContainer(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                duration: Duration(milliseconds: 500),
+                decoration: BoxDecoration(
+                    border:
+                        Border.all(color: CoresClaras.cinzaBordas, width: 2),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: Padroes.aluturaBotoes(),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Formação',
+                            style: estiloTexto(15),
+                          ),
+                          Transform.rotate(
+                            angle: formacao ? 3.14 : 0,
+                            child: Iconify(
+                              Icones.setaBaixo,
+                              color: CoresClaras.cinzaBordas,
+                              size: 36,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 500),
+                      child: formacao
+                          ? SizedBox(
+                              height:
+                                  formacao ? Padroes.alturaGeral() * 0.07 : 0,
+                              child: ListView.builder(
+                                physics: Padroes.efeitoDeRolagem(),
+                                scrollDirection: Axis.horizontal,
+                                itemCount: provider.listaNivelFormacao.length,
+                                itemBuilder: (context, index) => Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 15),
+                                  margin: EdgeInsets.only(bottom: 15),
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: CoresClaras.cinzaBordas),
+                                  ),
+                                  child: Text(provider.listaNivelFormacao[index]
+                                      .ofertaFormacao['nome']!),
+                                ),
+                              ),
+                            )
+                          : null,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
