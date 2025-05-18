7225243f637588d3dd901625f6ec41fc0e737031
Yuri Gabriel <yurigabrielsouza10@gmail.com>
2025-05-18 17:55:01 -0400

    style: estilizando o botão de entrar e os cards selecionaveis :gem:

--- sisgha/lib/app/views/aluno/selecionar_turma/widgets/teste.dart
+++ sisgha/lib/app/views/aluno/selecionar_turma/widgets/teste.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sisgha/app/core/utils/colors.dart';
 import 'package:sisgha/app/core/utils/estilos.dart';
 import 'package:sisgha/app/core/utils/icones.dart';
 import 'package:sisgha/app/core/utils/padroes.dart';
+import 'package:sisgha/app/providers/escolha_horarios_alunos.dart';
 import 'package:sisgha/app/views/aluno/selecionar_turma/widgets/tests/card_curso.dart';
 import 'package:sisgha/app/views/aluno/selecionar_turma/widgets/tests/card_formacao.dart';
 import 'package:sisgha/app/views/aluno/selecionar_turma/widgets/tests/card_turma.dart';
@@ -16,6 +19,14 @@ class Teste extends StatefulWidget {
 class _TesteState extends State<Teste> {
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<EscolhaHorariosAlunos>(context);
+
+    bool botaoAtivo = provider.idFormacaoSelecionada != null &&
+            provider.cursoSelecionado != null &&
+            provider.turmaSelecionada != null
+        ? true
+        : false;
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -45,10 +56,33 @@ class _TesteState extends State<Teste> {
             SizedBox(height: 20),
             RepaintBoundary(child: CardTurma()),
             SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: Text('Ver Horário')),
+            ElevatedButton(
+              style: buttonStyle(),
+              onPressed: () => botaoAtivo
+                  ? Navigator.of(context).pushNamed('/navegação')
+                  : null,
+              child: Text(
+                'Ver Horário',
+                style: estiloTexto(16,
+                    cor: CoresClaras.brancoTexto, peso: FontWeight.bold),
+              ),
+            ),
           ],
         ),
       ],
+    );
+  }
+
+  ButtonStyle buttonStyle() {
+    return ButtonStyle(
+      minimumSize: WidgetStatePropertyAll(
+          Size(double.infinity, Padroes.aluturaBotoes())),
+      maximumSize: WidgetStatePropertyAll(
+          Size(double.infinity, Padroes.aluturaBotoes())),
+      backgroundColor: WidgetStatePropertyAll(CoresClaras.verdePrincipalBotao),
+      shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+          side: BorderSide(color: CoresClaras.verdePrincipalBorda),
+          borderRadius: BorderRadiusGeometry.circular(15))),
     );
   }
 }
