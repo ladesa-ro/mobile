79e9c1ed9b7e293784f33674c82465bbf8f9dd64
victor henrique <victorcardosoderreirahenrique@gmail.com>
2025-08-07 16:18:16 -0400

    refactor: modal listagem de eventos finalizado  :hammer:
    finalizando o model de listagem dos eventos dos alunos agr só vai falta o dos professores

--- sisgha/lib/app/views/aluno/calendario/widgets/modal_eventos_alunos.dart
+++ sisgha/lib/app/views/aluno/calendario/widgets/modal_eventos_alunos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sisgha/app/core/utils/colors.dart';
 
 class ModalEventosAlunos extends StatefulWidget {
   const ModalEventosAlunos({super.key});
@@ -10,6 +11,8 @@ class ModalEventosAlunos extends StatefulWidget {
 class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
   final FocusNode _focusNode = FocusNode();
   final TextEditingController _controller = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+
   bool _isFocused = false;
   bool _isEmpty = true;
 
@@ -34,6 +37,7 @@ class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
   void dispose() {
     _focusNode.dispose();
     _controller.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
@@ -42,11 +46,11 @@ class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
     return Align(
       alignment: Alignment.bottomCenter,
       child: FractionallySizedBox(
-        heightFactor: 0.6, // 60% da altura da tela
-        widthFactor: 0.85, // 80% da largura do modal
+        heightFactor: 0.6,
+        widthFactor: 0.90,
         child: Container(
           decoration: const BoxDecoration(
-            color: Colors.white,
+            color: CoresClaras.branco,
             borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
           ),
           padding: const EdgeInsets.only(top: 16),
@@ -64,7 +68,7 @@ class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
                         Container(
                           width: 4,
                           height: 24,
-                          color: Colors.green,
+                          color: CoresClaras.verdeBordas,
                         ),
                         const SizedBox(width: 8),
                         const Text(
@@ -100,19 +104,24 @@ class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
                               : 'Digite o nome do evento ...',
                           suffixIcon: Icon(
                             Icons.search,
-                            color: _isFocused ? Colors.green : Colors.grey,
+                            color: _isFocused
+                                ? CoresClaras.verdeBordas
+                                : CoresClaras.cinza,
                           ),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(color: Colors.grey),
+                            borderSide:
+                                const BorderSide(color: CoresClaras.cinza),
                           ),
                           enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(color: Colors.grey),
+                            borderSide:
+                                const BorderSide(color: CoresClaras.cinza),
                           ),
                           focusedBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(color: Colors.green),
+                            borderSide:
+                                const BorderSide(color: CoresClaras.cinza),
                           ),
                           contentPadding: const EdgeInsets.symmetric(
                               horizontal: 12, vertical: 16),
@@ -123,14 +132,16 @@ class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
                       left: 12,
                       top: 0,
                       child: Container(
-                        color: Colors.white,
+                        color: CoresClaras.branco,
                         padding: const EdgeInsets.symmetric(horizontal: 4),
                         child: Text(
                           'Pesquisar',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 14,
-                            color: _isFocused ? Colors.green : Colors.grey,
+                            color: _isFocused
+                                ? CoresClaras.verdeBordas
+                                : CoresClaras.cinza,
                           ),
                         ),
                       ),
@@ -144,10 +155,15 @@ class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
               // Lista de eventos
               Expanded(
                 child: RawScrollbar(
-                  thumbColor: Colors.green.shade400,
+                  controller: _scrollController,
+                  thumbVisibility: true,
+                  trackVisibility: true,
+                  thickness: 5,
                   radius: const Radius.circular(10),
-                  thickness: 6,
+                  trackColor: CoresClaras.verdeclarinho,
+                  thumbColor: CoresClaras.verdebotao,
                   child: ListView.builder(
+                    controller: _scrollController,
                     padding: const EdgeInsets.symmetric(horizontal: 16),
                     itemCount: 10,
                     itemBuilder: (context, index) {
@@ -155,7 +171,7 @@ class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
                         margin: const EdgeInsets.only(bottom: 12),
                         padding: const EdgeInsets.all(12),
                         decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey.shade300),
+                          border: Border.all(color: CoresClaras.cinza),
                           borderRadius: BorderRadius.circular(10),
                         ),
                         child: Column(
@@ -173,9 +189,12 @@ class _ModalEventosAlunosState extends State<ModalEventosAlunos> {
                                         fontWeight: FontWeight.bold)),
                               ],
                             ),
-                            const SizedBox(height: 4),
+                            const SizedBox(height: 8),
                             const Text('Início: 08/02'),
                             const Text('Término: 23/03'),
+                            const SizedBox(
+                              height: 10,
+                            ),
                             const Text('Encerrado'),
                           ],
                         ),
