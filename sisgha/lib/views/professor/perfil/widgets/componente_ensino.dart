6c2b63233a3020147c06236d22649ea25f43e09d
Yuri Gabriel Ferreira de Souza <yurigabrielsouza10@gmail.com>
2025-08-17 20:02:45 -0400

    test: testando como os dados da rota ensino vão ser mostrados na tela :rotating_light:

--- sisgha/lib/views/professor/perfil/widgets/componente_ensino.dart
+++ sisgha/lib/views/professor/perfil/widgets/componente_ensino.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'package:sisgha/domain/model/dados_ensino.dart';
+import 'package:sisgha/viewmodels/dados_ensino_professor_provider.dart';
 import '../../../../core/utils/colors.dart';
 import '../../../../core/utils/estilos.dart';
 import '../../../../core/utils/padroes.dart';
@@ -13,47 +15,10 @@ class WidgetEnsino extends StatefulWidget {
 }
 
 class _WidgetEnsino extends State<WidgetEnsino> {
-  final List<Map<String, dynamic>> materias = [
-    {
-      "materia": "Matemática",
-      "cursos": [
-        {
-          "curso": "Informática",
-          "anos": ["1ºA", "1ºB", "2ºA", "3ºA"],
-        },
-        {
-          "curso": "Quimica",
-          "anos": ["1ºA", "2ºA", "3ºA"],
-        },
-        {
-          "curso": "Floresta",
-          "anos": ["1ºA", "2ºA", "3ºA"],
-        }
-      ]
-    },
-    {
-      "materia": "Português",
-      "cursos": [
-        {
-          "curso": "Informática",
-          "anos": ["1ºA", "1ºB", "2ºA", "3ºA"],
-        },
-        {
-          "curso": "Quimica",
-          "anos": ["1ºA", "2ºA", "3ºA"],
-        },
-      ]
-    },
-    {
-      "materia": "Geografia",
-      "cursos": [
-        {
-          "curso": "Informática",
-          "anos": ["1ºA", "1ºB", "2ºA", "3ºA"],
-        },
-      ]
-    },
-  ];
+  int cursoIndex = 0;
+  CarouselSliderController turmasCarouselController =
+      CarouselSliderController();
+
   int paginaMostrada = 0;
   void _atualizarIndexDaPaginaMostrada(index) {
     setState(() {
@@ -70,120 +35,108 @@ class _WidgetEnsino extends State<WidgetEnsino> {
           SizedBox(
             height: constraints.maxHeight * 0.05,
           ),
-          CarouselSlider(
-            items: List.generate(materias.length,
-                (index) => gerador(constraints.maxWidth, index)),
-            options: CarouselOptions(
-              onPageChanged: (index, reason) =>
-                  _atualizarIndexDaPaginaMostrada(index),
-              enableInfiniteScroll: false,
-              viewportFraction: 0.8,
-              enlargeCenterPage: false,
-              height: constraints.maxHeight * 0.50,
-            ),
-          ),
+          Consumer<DadosEnsinoProfessorProvider>(
+              builder: (context, provider, child) {
+            final listaDados = provider.dadosEnsino;
+
+            return CarouselSlider(
+              items: listaDados.map((dado) {
+                return Builder(builder: (context) {
+                  return Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                            color: CoresClaras.cinzaBordas, width: 2)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        mostrarImagem(
+                            constraints.maxWidth, constraints.maxHeight * 0.15),
+                        Divider(
+                          height: 0,
+                          color: CoresClaras.cinzaBordas,
+                          thickness: 2,
+                        ),
+                        Padding(
+                          padding: EdgeInsetsGeometry.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                dado.disciplina.nome,
+                                style: estiloTexto(16,
+                                    cor: CoresClaras.pretoTexto,
+                                    peso: FontWeight.bold),
+                              ),
+                              Container(
+                                height: constraints.maxHeight * 0.2,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  border: Border.all(
+                                      color: CoresClaras.cinzaBordas, width: 2),
+                                ),
+                                child: mostrarInformacoes(dado.cursos),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                });
+              }).toList(),
+              options: CarouselOptions(
+                onPageChanged: (index, reason) =>
+                    _atualizarIndexDaPaginaMostrada(index),
+                enableInfiniteScroll: false,
+                viewportFraction: 1,
+                enlargeCenterPage: false,
+                height: constraints.maxHeight * 0.50,
+              ),
+            );
+          }),
           SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(
-              materias.length,
-              (index) => _animatedContainer(index),
-            ),
-          ),
         ],
       ),
     );
   }
 
-  Widget gerador(double largura, int index) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      width: double.infinity,
-      decoration: estiloBorda(cor: CoresClaras.verdecinzaBorda, radius: 10),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(9),
-              topRight: Radius.circular(9),
-            ),
-            child: Image.asset(
-              'assets/img/gtr.jpeg',
-              fit: BoxFit.cover,
-              alignment: Alignment.bottomCenter,
-              width: largura,
-              height: 70,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    materias[index]["materia"],
-                    style: estiloTexto(14, peso: FontWeight.bold),
-                  ),
-                ),
-                _mostrarInformacoesdaMateria(index),
-              ],
-            ),
-          ),
-        ],
+  Widget mostrarImagem(double width, double height) {
+    return ClipRRect(
+      borderRadius: BorderRadiusGeometry.only(
+          topLeft: Radius.circular(14), topRight: Radius.circular(14)),
+      child: Image.asset(
+        'assets/img/gtr.jpeg',
+        height: height,
+        width: width,
+        fit: BoxFit.cover,
       ),
     );
   }
 
-  Widget _mostrarInformacoesdaMateria(int index) {
+  Widget mostrarInformacoes(List<CursoComTurmas> dados) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: materias[index]["cursos"].map<Widget>((curso) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4),
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            decoration: BoxDecoration(
-              border: Border.all(color: CoresClaras.verdecinza),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(
-              children: [
-                Text(
-                  curso["curso"],
-                  style: estiloTexto(14, peso: FontWeight.bold),
-                ),
-                const Spacer(),
-                Text(
-                  curso["anos"].join(", "),
-                  style: estiloTexto(14, peso: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-        );
-      }).toList(),
-    );
-  }
-
-  AnimatedContainer _animatedContainer(int index) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 100),
-      margin: const EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: paginaMostrada == index
-              ? CoresClaras.verdePrincipalBorda
-              : CoresClaras.verdecinzaBorda,
+      children: [
+        Row(
+          children: [Text(dados[cursoIndex].curso.nome)],
         ),
-        borderRadius: BorderRadius.circular(100),
-        color: paginaMostrada == index
-            ? CoresClaras.verdePrincipal
-            : CoresClaras.branco,
-      ),
-      width: 15,
-      height: 15,
+        Divider(),
+        CarouselSlider(
+            carouselController: turmasCarouselController,
+            items: dados[cursoIndex].turmas.map((turma) {
+              return Text(turma.nome);
+            }).toList(),
+            options: CarouselOptions(
+              enableInfiniteScroll: false,
+              viewportFraction: 1.0,
+              enlargeCenterPage: true,
+              height: 10,
+              scrollPhysics: const NeverScrollableScrollPhysics(),
+            ))
+      ],
     );
   }
 }
