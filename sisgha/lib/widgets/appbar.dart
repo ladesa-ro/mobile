21d5cb3a6bfc04ec3481d17dd853f4398296c2c3
Yuri Gabriel Ferreira de Souza <yurigabrielsouza10@gmail.com>
2025-09-27 18:30:29 -0400

    style: arrumado estilização do appbar dos professores :gem:

--- sisgha/lib/widgets/appbar.dart
+++ sisgha/lib/widgets/appbar.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:sisgha/core/theme/tema_provider.dart';
 import 'package:sisgha/core/utils/colors.dart';
 import 'package:sisgha/core/utils/dias.dart';
 import 'package:sisgha/core/utils/estilos.dart';
 import 'package:sisgha/core/utils/icones.dart';
 import 'package:sisgha/core/utils/padroes.dart';
 import 'package:sisgha/viewmodels/escolha_horarios_alunos.dart';
+import 'package:sisgha/widgets/dialogo_troca_de_tema.dart';
 import 'package:sizer/sizer.dart';
 
 PreferredSizeWidget appBar(BuildContext ctx, EscolhaHorariosAlunos? provInfo) {
+  var temaAtivo = Provider.of<TemasProvider>(ctx).temaAtivo.brightness;
+
+  String verificarTemaAtivo() {
+    if (temaAtivo == Brightness.light) return 'escuro';
+    return 'claro';
+  }
+
   return AppBar(
       toolbarHeight: 7.h,
       title: Container(
@@ -16,10 +26,10 @@ PreferredSizeWidget appBar(BuildContext ctx, EscolhaHorariosAlunos? provInfo) {
         child: Row(
           children: [
             provInfo == null
-                ? Container()
+                ? Padding(padding: EdgeInsets.only(left: 2.w))
                 : IconButton(
-                    onPressed: () {}, icon: Iconify(Icones.setaVoltarDireita)),
-            SizedBox(width: 2.w),
+                    onPressed: () {},
+                    icon: _buildIcones(Icones.setaVoltarDireita, 23)),
             _buildInfo(),
             SizedBox(width: 2.w),
             Column(
@@ -27,13 +37,21 @@ PreferredSizeWidget appBar(BuildContext ctx, EscolhaHorariosAlunos? provInfo) {
               children: [_buildTitulo(), _buildSubtitulo()],
             ),
             const Spacer(),
-            IconButton(onPressed: () {}, icon: Iconify(Icones.sol)),
+            IconButton(
+                onPressed: () {
+                  showDialog(
+                    context: ctx,
+                    builder: (context) => mostrarDialogoDeTrocaDeTema(
+                        context, verificarTemaAtivo()),
+                  );
+                },
+                icon: _buildIcones(Icones.sol, 22)),
             provInfo == null
                 ? IconButton(
                     onPressed: () {
                       Navigator.of(ctx).pushNamed('/notificacao');
                     },
-                    icon: Iconify(Icones.sino))
+                    icon: _buildIcones(Icones.sino, 22))
                 : Container()
           ],
         ),
@@ -62,4 +80,12 @@ Widget _buildSubtitulo() {
   return Text('${Padroes.primeiraLetraMaiuscula(mes)} - Dia $primDia a $ultDia',
       style:
           estiloTexto(16, cor: CoresClaras.brancoTexto, peso: FontWeight.bold));
+}
+
+Iconify _buildIcones(String icone, double size) {
+  return Iconify(
+    icone,
+    color: CoresClaras.branco,
+    size: size.sp,
+  );
 }
